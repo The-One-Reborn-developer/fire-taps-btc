8580e09bc4a23977bd174ed367168186a6a8c725
The One Reborn <mortonmarrion@gmail.com>
2024-10-26 08:47:30 +0300

    fix: delete reply keyboard message after sending

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -11,6 +11,8 @@ profile_router = Router()
 
 @profile_router.message(F.text == 'Профиль 👤')
 async def profile(message: Message) -> None:
+    await message.delete()
+    
     user = await get_user(message.from_user.id)
     if user[1] is None:
         btc_balance = 0
@@ -21,6 +23,6 @@ async def profile(message: Message) -> None:
         content = f'Пользователь: {message.from_user.first_name}\n\n' \
                   f'BTC Баланс: {btc_balance} ₿\n\n' \
                   f'Количество зарегистрированных рефералов: {referrals_amount}\n\n' \
-                  f'Код реферала (для регистрации): {user[3]}'
+                  f'Код реферала (для регистрации): <code>{user[3]}</code>'
 
-        await message.answer(content, reply_markup=withdraw_keyboard())
+        await message.answer(content, reply_markup=withdraw_keyboard(), parse_mode='HTML')
