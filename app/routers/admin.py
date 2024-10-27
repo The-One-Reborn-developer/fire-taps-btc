570505726ce118cd19e2d233fb25a0c7210b51a6
The One Reborn <mortonmarrion@gmail.com>
2024-10-27 10:39:32 +0300

    feat: generate referral for playing

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -6,6 +6,8 @@ from app.database.queues.get_user import get_user
 
 from app.bot.get_balance import get_balance
 
+from app.generators.play_referral import play_referral
+
 from app.keyboards.admin import admin_keyboard
 from app.keyboards.main import main_keyboard
 
@@ -23,8 +25,12 @@ async def admin_panel(message: Message) -> None:
 
             balance = await get_balance()
 
+            with open('app/temp/play_referral_code.txt', 'r') as f:
+                play_referral_code = f.read()
+
             content = 'Вход в панель администратора 🔑\n' \
-                      f'Баланс BTC кошелька приложения: {balance} ₿'
+                      f'Баланс BTC кошелька приложения: {balance} ₿\n' \
+                      f'Текущий реферальный код для игры: {play_referral_code}'
 
             await message.answer(content, reply_markup=admin_keyboard())
         else:
@@ -41,11 +47,36 @@ async def exit_admin_panel(message: Message) -> None:
         if user[5] is True:
             await message.delete()
 
-            await message.answer('Вы вышли из панели администратора', reply_markup=main_keyboard())
+            await message.answer('Вы вышли из панели администратора ✅', reply_markup=main_keyboard())
         else:
             pass
     except Exception as e:
         print(f'Exit admin panel error: {e}')
+
+
+@admin_router.message(F.text == 'Сгенерировать реф. код для игры 🎁')
+async def generate_referral_code(message: Message) -> None:
+    try:
+        user = await get_user(message.from_user.id)
+
+        if user[5] is True:
+            await message.delete()
+
+            await play_referral()
+
+            balance = await get_balance()
+
+            with open('app/temp/play_referral_code.txt', 'r') as f:
+                play_referral_code = f.read()
+
+            content = f'Баланс BTC кошелька приложения: {balance} ₿\n' \
+                      f'Текущий реферальный код для игры: {play_referral_code}'
+
+            await message.answer(content, reply_markup=admin_keyboard())
+        else:
+            pass
+    except Exception as e:
+        print(f'Generate referral code error: {e}')
 
 
 @admin_router.message(F.text == 'Пополнить BTC кошелёк ₿')
